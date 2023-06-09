342f3a6231f3764ddb9bcf8738be31460f43458e
Screin208 <snay208@gmail.com>
2023-06-09 22:32:12 +0000

    ejercicio

---
+++ index.js
@@ -0,0 +1,55 @@
+const simbolos = ["corazon", "diamante", "pica", "trebol"];
+const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let simboloArriba = document.querySelector("#simboloArriba");
+let numero = document.querySelector("#numero");
+let simboloAbajo = document.querySelector("#simboloAbajo");
+
+
+function eleccionCarta(arr1, arr2){
+    let carta =[]
+    carta.push(arr1[Math.floor(Math.random()*arr1.length)]);
+    carta.push(arr2[Math.floor(Math.random()*arr2.length)]);
+    return carta
+}
+function asignarColor(arr){
+    if(arr[0] === "corazon" || arr[0] === "diamante"){
+        simboloArriba.style.color = "red";
+        numero.style.color = "red";
+        simboloAbajo.style.color = "red";
+    }
+    else{
+        simboloArriba.style.color = "black";
+        numero.style.color = "black";
+        simboloAbajo.style.color = "black";
+    }
+}
+function asignarSimbolo(arr){
+    if(arr[0] === "corazon"){
+        simboloArriba.innerHTML = "♥"
+        simboloAbajo.innerHTML = "♥"
+    }
+    else if(arr[0] === "diamante"){
+        simboloArriba.innerHTML = "♦"
+        simboloAbajo.innerHTML = "♦"
+    }
+    else if(arr[0] === "trebol"){
+        simboloArriba.innerHTML = "♣"
+        simboloAbajo.innerHTML = "♣"
+    }
+    else{
+        simboloArriba.innerHTML = "♠"
+        simboloAbajo.innerHTML = "♠"
+    }
+}
+function asignarValor(arr){
+    numero.innerHTML = arr[1];
+}
+function generacionCarta(){
+    let carta = eleccionCarta(simbolos, valores);
+    asignarColor(carta);
+    asignarSimbolo(carta);
+    asignarValor(carta);
+}
+
+window.onload = function(){generacionCarta()}
+document.querySelector(".boton").addEventListener("click", generacionCarta)
